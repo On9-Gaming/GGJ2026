1db76b1df6b01533bddacdacd54e71a8ac3fdf20
Zuyi53 <rockmanzuyi@live.cn>
2026-02-01 12:58:55 +0800

    0.1

--- Source/GGJ2026/GGJ2026.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GGJ2026.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ2026, "GGJ2026" );
